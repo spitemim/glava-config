d33e3e1dceedf80abc7a57b47a703ce81393b1fb
spitemim <69088069+spitemim@users.noreply.github.com>
2020-11-10 23:33:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 glava --copy-config
 curl https://raw.githubusercontent.com/spitemim/glava-config/main/rc.glsl > ~/.config/glava/rc.glsl
 curl https://raw.githubusercontent.com/spitemim/glava-config/main/bars.glsl > ~/.config/glava/bars.glsl
+xmessage "Done."
