3cb323fa7b540ce6f379f2fc43ddd53513ee6d83
spitemim <69088069+spitemim@users.noreply.github.com>
2020-11-10 23:42:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+echo 'Downloading GLava source code...'
+git clone https://github.com/jarcode-foss/glava
+cd glava
+echo 'Building GLava...'
+meson build --prefix /usr
+ninja -C build
+sudo ninja -C build install
+sudo ldconfig
+echo 'Configuring...'
 glava --copy-config
 curl https://raw.githubusercontent.com/spitemim/glava-config/main/rc.glsl > ~/.config/glava/rc.glsl
 curl https://raw.githubusercontent.com/spitemim/glava-config/main/bars.glsl > ~/.config/glava/bars.glsl
+echo 'Installation complete.'
